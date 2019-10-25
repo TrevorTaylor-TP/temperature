aa971ac58d3cb976d403b74dcc49c7baba6d2695
TrevorTaylor-TP <ttaylor@talentpath.com>
2019-10-25 15:54:48 -0500

    Updated TemperatureInput, added convertion funcs

---
+++ src/TemperatureInput.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./App.css";
+
+const scaleNames = {
+	c: "Celsius",
+	f: "Fahrenheit"
+};
+
+class TemperatureInput extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(e) {
+		this.props.onTemperatureChange(e.target.value);
+	}
+
+	render() {
+		const temperature = this.props.temperature;
+		const scale = this.props.scale;
+		return (
+			<fieldset>
+				<legend>Enter temperature in {scaleNames[scale]}:</legend>
+				<input value={temperature} onChange={this.handleChange} />
+			</fieldset>
+		);
+	}
+}
+
+function toCelsius(fahrenheit) {
+	return (fahrenheit - 32) * (5 / 9);
+}
+
+function toFahrenheit(celsius) {
+	return celsius * (9 / 5) + 32;
+}
+
+function tryConvert(temperature, convert) {
+	const input = parseFloat(temperature);
+	if (Number.isNaN(input)) {
+		return "";
+	}
+	const output = convert(input);
+	const rounded = Math.round(output * 1000) / 1000;
+	return rounded.toString();
+}
+
+export default TemperatureInput;
